c110effbaf62d83111959391e504c4e431b06054
ajinkya6373 <72693966+ajinkya6373@users.noreply.github.com>
2022-08-06 13:13:10 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,70 @@
+const readlineSync = require("readline-sync");
+
+let user
+const greeting =()=>{
+console.log("Lets play a game ");
+console.log("**************************************");
+user = readlineSync.question("What is your name ? ");
+console.log("");
+}
+
+const HighScorers =[
+
+  {
+    name:"ajinkya",
+    score:5
+    
+  }
+  
+]
+
+const questionsArray = [
+  {
+    question: "What does IPL stand for?",
+    answer: "Indian premier League"
+  },
+  {
+    question: "Which form of cricket restrict to a maximum of 20 overs?",
+    answer: "t20"
+  },
+  {
+    question: "Which famous cricketer now serves as Prime Minister of Pakistan (as of 2021)?",
+    answer: "Imran Khan"
+  },
+  {
+    question: "When did England win the Cricket World Cup for the first time (year)?",
+    answer: "2019"
+  },
+  {
+    question: "How many players are in a cricket team ?",
+    answer: "11"
+  },
+]
+
+
+let score = 0;
+
+const play =()=>{
+  for (let i = 0; i < questionsArray.length; i++) {
+  const userAns = readlineSync.question(questionsArray[i].question)
+  if (userAns.toLowerCase() === questionsArray[i].answer.toLowerCase()) {
+    console.log("You are right!!");
+    console.log("");
+    score++;
+
+  } else {
+    console.log("Sorry ,You are wrong !!!");
+    console.log("");
+  }
+
+  console.log("current score is :", score);
+}
+}
+
+
+greeting()
+play()
+console.log("**************************************");
+console.log(user + " your score is " + score);
+console.log("**************************************");
+console.log("Highest Scorer is ", HighScorers[0].name)
